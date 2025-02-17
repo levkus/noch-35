c1e2b60d33c2a5150a13e0c2d16f568820cea5af
levkus <levkus@gmail.com>
2025-02-17 18:02:47 +0300

    fix index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
+import { Logo } from "@/components/Logo";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-md shadow-lg max-w-md w-full">
-        <h1 className="text-2xl font-bold text-red-600 mb-4">Ошибка доступа</h1>
-        <p className="text-gray-700">
-          Для доступа к форме используйте персональную ссылку, которую вы
-          получили в приглашении.
-        </p>
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <Logo className="w-[300px] mb-4" />
     </div>
   );
 }
